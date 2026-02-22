20794fc721a1d1f41d5de383d533b0aa7577491e
lotharsky <hennelothar@gmail.com>
2026-02-22 19:50:45 +0100

    finish playGame function and whole game

--- script.js
+++ script.js
@@ -20,19 +20,55 @@ const getHumanChoice = () => {
     return choice.toLowerCase()
 }
 
-
-// score variables
-let humanScore = 0
-let computerScore = 0
-
-
-// Game round
+//Play a round
 const playRound = (humanChoice, computerChoice) => {
-    
+    if(humanChoice === computerChoice) {
+        console.log("draw")
+        }
+    else if ((humanChoice === "rock" && computerChoice === "scissor") || (humanChoice === "scissor" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")) {
+       
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        return "human"
+    } 
+    else {
+        
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        return "computer"
+    }
 }
 
-const humanSelection = getComputerChoice()
-const computerSelection = getComputerChoice()
 
 
-playRound(humanSelection, computerSelection)
+const playGame = () => {
+
+    let humanScore = 0
+    let computerScore = 0
+
+    console.log("game started")
+    while (humanScore < 3 && computerScore < 3) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+
+        console.log("human: ", humanSelection)
+        console.log("computer: ", computerSelection)
+
+
+        const result =  playRound(humanSelection, computerSelection)
+
+        if(result === "human") {
+            humanScore ++
+        } else if (result ==="computer") {
+            computerScore ++
+        }
+
+        console.log("HS: ", humanScore, "PCS: ", computerScore)
+    }
+
+    console.log("game ended")
+}
+
+playGame()
+
+
+
+
